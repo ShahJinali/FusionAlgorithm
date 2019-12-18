1cc76f19d45e721c9ad821e82ada53d0f82cd426
ShahJinali <shahjinali96@gmail.com>
2019-12-17 22:36:41 -0500

    Updated the validate sensor

--- src/validate_sensor.c
+++ src/validate_sensor.c
@@ -1,17 +1,25 @@
+/**
+ * @file validate_sensor.c
+ * @author Shah Jinali <jinalibenhiteshbhais@cmail.carleton.ca>
+ * @author Shah Rushabh <RushabhSudhirkumarSh@cmail.carleton.ca>
+ * @author Patel Jay <jpate122@uottawa.ca>
+ */
 #include <time_structure.h>
 #include <sensor_structure.h>
-#include <stdio.h>
 #include <unique_time.h>
 #include <string.h>
 /**
  * <p>This function computes the number of sensors at a particular time and filter the data of out of range sensors.</p>
- * @param p_sensor
- * @param min
- * @param max
- * @param line_counter
- * @param curr_time
- * @param v_sensor
- * @version 1.0
+ * @param[in] p_sensor
+ * @param[in] min
+ * @param[in] max
+ * @param[in] line_counter
+ * @param[in] curr_time
+ * @param[out] v_sensor
+ * @return It will return the no of sensors which are within the specified range.
+ * @brief This function finds the sensors which are out of range. The range is specified by user from command line.
+ * It will return the no of sensors which are within the specified range and also stores the data of valid sensor into v_sensor.
+ * This new no of sensors and v_sensor are passed into fusion algorithm.
  */
 int compare_sensor_range(sensor_t *p_sensor,double min ,double max,int line_counter,time_tt curr_time,sensor_t *v_sensor){
 
